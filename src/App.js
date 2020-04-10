d9fef04549ec02b3652834289066bf9132d6d1f5
Varun Desai <varundesai@Varuns-MacBook-Pro.local>
2020-04-09 22:32:40 -0700

    Shareable Apps added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import AllEventsPage from "./Components/allEventsPage";
 import createEvents from "./Components/createEvents";
 import IndividualEventsPage from "./Components/individualEventsPage";
 import TopBar from "./Components/TopBar";
+import ExtenderApp from "./Components/ExtenderApp"
 import "./App.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
---
+++ src/Components/ExtenderApp.js
@@ -0,0 +1,111 @@
+import React, { Component } from 'react';
+import {
+  
+  FacebookShareButton,
+  FacebookMessengerShareButton,
+  FacebookMessengerIcon,
+  LinkedinShareButton,
+  TwitterShareButton,
+  WhatsappShareButton,
+  RedditShareButton,
+  EmailShareButton,
+  FacebookIcon,
+  TwitterIcon,
+  LinkedinIcon,
+  WhatsappIcon,
+  RedditIcon,
+  EmailIcon,
+  
+} from "react-share";
+
+import "../CSS/createEvents.css";
+
+
+class ExtenderApp extends Component {
+  render() {
+    const shareUrl = 'https://jyeh20.github.io/hyprlink/';
+    const title = 'GitHub';
+
+    return (
+      <div className="Demo__container">
+      
+        <div className="Demo__some-network">
+          <TwitterShareButton
+            url={shareUrl}
+            title={title}
+            className="Demo__some-network__share-button"
+          >
+            <TwitterIcon size={32} round />
+          </TwitterShareButton>
+
+          <div className="Demo__some-network__share-count">&nbsp;</div>
+        </div>
+
+        <div className="Demo__some-network">
+          <WhatsappShareButton
+            url={shareUrl}
+            title={title}
+            separator=":: "
+            className="Demo__some-network__share-button"
+          >
+            <WhatsappIcon size={32} round />
+          </WhatsappShareButton>
+
+          <div className="Demo__some-network__share-count">&nbsp;</div>
+        </div>
+
+        <div className="Demo__some-network">
+          <LinkedinShareButton url={shareUrl} className="Demo__some-network__share-button">
+            <LinkedinIcon size={32} round />
+          </LinkedinShareButton>
+        </div>
+        <div className="Demo__some-network">
+          <FacebookShareButton
+            url={shareUrl}
+            quote={title}
+            className="Demo__some-network__share-button"
+          >
+            <FacebookIcon size={32} round />
+          </FacebookShareButton>
+
+        </div>
+        <div className="Demo__some-network">
+          <RedditShareButton
+            url={shareUrl}
+            title={title}
+            windowWidth={660}
+            windowHeight={460}
+            className="Demo__some-network__share-button"
+          >
+            <RedditIcon size={32} round />
+          </RedditShareButton>
+
+          
+        </div>
+        <div className="Demo__some-network">
+          <EmailShareButton
+            url={shareUrl}
+            subject={title}
+            body="body"
+            className="Demo__some-network__share-button"
+          >
+            <EmailIcon size={32} round />
+          </EmailShareButton>
+        </div>
+
+        <div className="Demo__some-network">
+          <FacebookMessengerShareButton
+            url={shareUrl}
+            appId="521270401588372"
+            className="Demo__some-network__share-button"
+          >
+            <FacebookMessengerIcon size={32} round />
+          </FacebookMessengerShareButton>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ExtenderApp;
+    
